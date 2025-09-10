73e91dbb9c7d25639da500183ee769ffbcb03287
Reveck <cgngtr5026@hotmail.com>
2025-09-10 03:37:18 +0300

    style: Update StatisticsSection and TestimonialsSection for improved layout and responsiveness
    This commit enhances the StatisticsSection by increasing font sizes for better visibility and adjusting button states for improved user interaction. Additionally, the TestimonialsSection is refactored to optimize layout, including changes to padding and flex properties, ensuring a more cohesive design and better responsiveness across devices.

--- src/components/StatisticsSection.tsx
+++ src/components/StatisticsSection.tsx
@@ -6,7 +6,7 @@ const StatisticsSection = () => {
       <div className="rounded-lg border bg-card text-card-foreground shadow-sm max-md:h-full">
         <div className="flex flex-col items-center justify-center gap-1 p-3 md:p-4">
           <div className="flex items-center font-bold">
-            <span className="bg-gradient-to-b from-white to-gray-800 bg-clip-text font-mono text-3xl text-transparent md:text-4xl">19</span>
+            <span className="bg-gradient-to-b from-white to-gray-800 bg-clip-text font-mono text-4xl text-transparent md:text-5xl">19</span>
             <svg xmlns="http://www.w3.org/2000/svg" className="iconify iconify--mingcute size-6 text-primary md:size-8" width="1em" height="1em" viewBox="0 0 24 24">
               <g fill="none" fillRule="evenodd">
                 <path d="m12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036q-.016-.004-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z"></path>
@@ -14,17 +14,12 @@ const StatisticsSection = () => {
               </g>
             </svg>
           </div>
-          <button data-state="closed" className="cursor-help">
+          <button data-state="delayed-open" className="cursor-default" aria-describedby="radix-:r0:">
             <div className="relative flex w-fit items-center justify-center gap-1 rounded-xl bg-secondary/40 px-3 py-1 md:px-3 md:py-1.5">
               <svg xmlns="http://www.w3.org/2000/svg" className="iconify iconify--ic size-6 text-primary" width="1em" height="1em" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M20 6h-4V4c0-1.11-.89-2-2-2h-4c-1.11 0-2 .89-2 2v2H4c-1.11 0-1.99.89-1.99 2L2 19c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V8c0-1.11-.89-2-2-2m-6 0h-4V4h4z"></path>
               </svg>
               <span className="text-sm font-semibold text-gray-400">Projects</span>
-              <div className="absolute right-0 top-0 flex size-6 -translate-y-2 translate-x-2 items-center justify-center rounded-full bg-primary">
-                <svg xmlns="http://www.w3.org/2000/svg" className="iconify iconify--fluent size-4 text-background" width="1em" height="1em" viewBox="0 0 16 16">
-                  <path fill="currentColor" d="M1 5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v5a2 2 0 0 1-1.164 1.818a1.5 1.5 0 0 0-.275-.379l-4-4A1.5 1.5 0 0 0 7 8.5V12H3a2 2 0 0 1-2-2zm7.854 3.146A.5.5 0 0 0 8 8.5v6a.5.5 0 0 0 .9.3l1.35-1.8h2.25a.5.5 0 0 0 .354-.854z"></path>
-                </svg>
-              </div>
             </div>
           </button>
         </div>
@@ -32,7 +27,7 @@ const StatisticsSection = () => {
       <div className="rounded-lg border bg-card text-card-foreground shadow-sm max-md:h-full">
         <div className="flex flex-col items-center justify-center gap-1 p-3 md:p-4">
           <div className="flex items-center font-bold">
-            <span className="bg-gradient-to-b from-white to-gray-800 bg-clip-text font-mono text-3xl text-transparent md:text-4xl">9</span>
+            <span className="bg-gradient-to-b from-white to-gray-800 bg-clip-text font-mono text-4xl text-transparent md:text-5xl">9</span>
             <svg xmlns="http://www.w3.org/2000/svg" className="iconify iconify--mingcute size-6 text-primary md:size-8" width="1em" height="1em" viewBox="0 0 24 24">
               <g fill="none" fillRule="evenodd">
                 <path d="m12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036q-.016-.004-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z"></path>
--- src/components/TestimonialsSection.tsx
+++ src/components/TestimonialsSection.tsx
@@ -11,19 +11,20 @@ interface TestimonialProps {
 
 const Testimonial = ({ name, company, text, blurred = false }: TestimonialProps) => {
   return (
-    <div className={`relative ${blurred ? 'mb-0' : 'mb-2'} bg-[#0d0c0d] p-3 rounded-xl flex flex-col space-y-1.5`}>
-      <div className="flex justify-between items-center">
-        <div>
-          <h3 className="font-bold text-[#fafafa]">{name}</h3>
-          <p className="text-xs text-muted-foreground font-bold">{company}</p>
+    <div className={`relative ${blurred ? 'mb-0' : 'mb-2'} bg-[#0d0c0d] rounded-xl flex flex-col h-full`}>
+      <div className="p-3 py-4 flex flex-col gap-2 flex-grow">
+        <div className="flex flex-col">
+          <h3 className="font-bold">{name}</h3>
+          <p className="text-sm font-semibold text-gray-400">{company}</p>
         </div>
-        <div className="flex">
-          {[1, 2, 3, 4, 5].map((star) => (
-            <Star key={star} className="w-4 h-4 text-[#b2a7fb]" fill="#b2a7fb" />
-          ))}
-        </div>
+        <p className="text-sm font-semibold text-gray-400 flex-grow">{text}</p>
       </div>
-      <p className="text-sm text-muted-foreground font-bold">{text}</p>
+      
+      <div className="absolute right-2 top-4 flex flex-row">
+        {[1, 2, 3, 4, 5].map((star) => (
+            <Star key={star} className="w-4 h-4 text-[#b2a7fb]" fill="#b2a7fb" />
+        ))}
+      </div>
       
       {blurred && (
         <div className="absolute inset-0 bg-gradient-to-b from-transparent via-[#0d0c0d]/60 to-[#0d0c0d] rounded-xl" />
@@ -43,40 +44,38 @@ const TestimonialsSection = () => {
         <p className="text-sm text-muted-foreground">What my clients say about me</p>
       </div>
       
-      <div className="p-4 pt-0">
-        <div className="relative overflow-hidden">
+      <div className="p-4 pt-4 -m-4">
+        <div className="relative max-h-64 overflow-hidden grid grid-cols-1 gap-3 p-4 pt-0">
+          <div className="absolute inset-x-0 bottom-0 z-50 h-52 w-full bg-gradient-to-b from-transparent to-[#151515]"></div>
+          <div className="absolute inset-x-0 bottom-5 z-50 flex items-center justify-center">
+            <a href="/testimonials">
+              <Button className="bg-[#b2a7fb]/10 border-none hover:bg-[#b2a7fb]/20 rounded-xl py-2 px-4">
+                <span className="text-[#b2a7fb] font-bold flex items-center">
+                  <Star className="w-5 h-5 mr-2 text-[#b2a7fb]" fill="#b2a7fb" />
+                  View All Testimonials
+                </span>
+              </Button>
+            </a>
+          </div>
           <Testimonial 
             name="Anom"
             company="Delvfox"
             text="Working with Çağan was a great experience. He is always on time and ready to help"
           />
           
-          <div className="relative max-h-24 overflow-hidden">
-            <Testimonial 
-              name="Anom"
-              company="NixServices"
-              text="This dude is a beast. He is very good at what he is doing I would recommend him to anyone who needs a website."
-              blurred={true}
-            />
-            
-            <Testimonial 
-              name="Emilio"
-              company="Addimax"
-              text="Great experience working with Çağan. Would hire again!"
-              blurred={true}
-            />
-          </div>
-        </div>
-        
-        <div className="flex justify-center pt-1">
-          <a href="/testimonials">
-            <Button className="bg-[#b2a7fb]/10 border-none hover:bg-[#b2a7fb]/20 rounded-xl py-2 px-4">
-              <span className="text-[#b2a7fb] font-bold flex items-center">
-                <Star className="w-5 h-5 mr-2 text-[#b2a7fb]" fill="#b2a7fb" />
-                View All Testimonials
-              </span>
-            </Button>
-          </a>
+          <Testimonial 
+            name="Anom"
+            company="NixServices"
+            text="This dude is a beast. He is very good at what he is doing I would recommend him to anyone who needs a website."
+            blurred={true}
+          />
+          
+          <Testimonial 
+            name="Emilio"
+            company="Addimax"
+            text="Great experience working with Çağan. Would hire again!"
+            blurred={true}
+          />
         </div>
       </div>
     </div>
